b8208c4d66db0725d44b0ef210da5c5119a13043
Berk ŞENEL <berksenel43@gmail.com>
2021-03-01 15:14:28 +0300

    Güncelleme

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -7,9 +7,12 @@ namespace Business.Constants
 {
     public static class Messages
     {
-        public static string ProductAdded = "Ürün eklendi";
-        public static string ProductNameInvalid = "Ürün ismi geçersiz";
-        public static string MaintenanceTime = "Sistem bakımda";
-        public static string ProductsListed = "Ürünler listelendi";
+        public static string ProductAdded = "Ürün eklendi.";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz!";
+        public static string MaintenanceTime = "Sistem bakımda!";
+        public static string ProductsListed = "Ürünler listelendi.";
+        public static string ProductCountOfCategoryError = "Bir kategoride en fazla 10 ürün olabilir!";
+        public static string ProductNameAlreadyExists = "Bu isimde bir ürün zaten var!";
+        internal static string CategoryLimitExceeded = "Kategori sayısı 15'i aştığı için sisteme yeni ürün eklenemez!";
     }
 }
